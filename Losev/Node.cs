a4e4ae70c2ccf94bdcd3b4b378e1f2872a7edd9c
LiGiR72 <iliya.alabyew@gmail.com>
2022-12-25 05:59:55 +0300

    Sorting end deletion added

---
+++ Losev/Node.cs
@@ -0,0 +1,87 @@
+﻿namespace Losev
+{
+    internal class BinaryTree
+    {
+        public Node? Root { get; set; }
+        public BinaryTree()
+        {
+            Root = null;
+        }
+        internal class Node
+        {
+            public int Key { get; set; }
+            public int Index { get; set; }
+            public Node? LTree { get; set; }
+            public Node? RTree { get; set; }
+
+            public Node(int key)
+            {
+                Key = key;
+                LTree = null;
+                RTree = null;
+            }
+        }
+        public void Add(int key) => Root = AddUtility(key, Root);
+        private Node AddUtility(int key, Node? node)
+        {
+            if (node == null)
+            {
+                node = new Node(key);
+                return node;
+            }
+            if (node.Key > key)
+            {
+                node.LTree = AddUtility(key, node.LTree);
+            }
+            else if (node.Key < key)
+            {
+                node.RTree = AddUtility(key, node.RTree);
+            }
+            return node;
+
+        }
+        public Node Delete(int key) => DeletionUtility(Root, key);
+
+        private Node DeletionUtility(Node? root, int key)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+            if (key < root.Key)
+                root.LTree = DeletionUtility(root.LTree, key);
+            else if (key > root.Key)
+                root.RTree = DeletionUtility(root.RTree, key);          
+            else
+            {              
+                if (root.LTree == null)
+                    return root.RTree;
+                else if (root.RTree == null)
+                    return root.LTree;
+                root.Key = minValue(root.RTree);
+                root.RTree = DeletionUtility(root.RTree, root.Key);
+            }
+            return root;
+        }
+        int minValue(Node? root)
+        {
+            int minv = root.Key;
+            while (root.LTree != null)
+            {
+                minv = root.LTree.Key;
+                root = root.LTree;
+            }
+            return minv;
+        }
+        public void inorder() => inorderRec(Root);
+        private void inorderRec(Node? root)
+        {
+            if (root != null)
+            {
+                inorderRec(root.LTree);
+                Console.WriteLine(root.Key);
+                inorderRec(root.RTree);
+            }
+        }
+    }
+}
--- Losev/Sorting.cs
+++ Losev/Sorting.cs
@@ -4,21 +4,43 @@
     {
         public static int[] Sort(int[] data)
         {
-           int n = data.Length;
-           int l = n / 2 + 1;
-           int r = n;
-           int j;
-           int[] output = new int[n];
-            if (l > 1)
-            {
-                l--;
-            }
-            else
-            {
 
+            int N = data.Length;
+
+            for (int i = N / 2 - 1; i >= 0; i--)
+                Heapify(data, N, i);
+
+            for (int i = N - 1; i > 0; i--)
+            {
+                Swap(0, i, ref data);
+                Heapify(data, i, 0);
             }
-            j = l;
-            
+            return data;
         }
+        private static void Swap(int a, int b, ref int[] data)
+        {
+            int temp;
+            temp = data[a];
+            data[a] = data[b];
+            data[b] = temp;
+
+        }
+
+        private static void Heapify(int[] arr, int N, int i)
+        {
+            int largest = i;
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+            if (l < N && arr[l] > arr[largest])
+                largest = l;
+            if (r < N && arr[r] > arr[largest])
+                largest = r;
+            if (largest != i)
+            {
+                Swap((int)largest, i, ref arr);            
+                Heapify(arr, N, largest);
+            }
+        }
+
     }
 }
